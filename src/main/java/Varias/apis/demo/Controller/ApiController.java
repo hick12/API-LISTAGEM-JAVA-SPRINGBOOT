cdccde451b3729aed230958abd428fee698daeca
Henrique Palorca <henriquepalorca@gmail.com>
2025-09-06 23:01:58 -0300

    Commit and push

---
+++ src/main/java/Varias/apis/demo/Controller/ApiController.java
@@ -0,0 +1,42 @@
+package Varias.apis.demo.Controller;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+public class ApiController {
+
+    private List<String> tasks = new ArrayList<>();
+    private ObjectMapper objectMapper;
+
+    public ApiController(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @GetMapping(path = "/task")
+    public ResponseEntity<String> Listtask() throws JsonProcessingException {
+        return ResponseEntity.ok(objectMapper.writeValueAsString(tasks));
+    }
+
+   @PostMapping (path = "/task")
+    public ResponseEntity <Void> creatTasks(@RequestBody String task)
+        {
+            tasks.add(task);
+            return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(path = "task")
+    public ResponseEntity<Void> clearTasks(){
+        tasks = new ArrayList<>();
+        return ResponseEntity.ok().build();
+
+    }
+
+}
---
+++ src/test/java/Varias/apis/demo/ApiDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package Varias.apis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
